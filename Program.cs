534c9832b5fd50306038a4ad36d268205c2a1240
Cerberus3445 <77907751+Cerberus3445@users.noreply.github.com>
2021-01-24 13:45:35 +1000

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿#region Автор
+//Егор Малов
+#endregion
+
+#region Описание задачи
+//Ввести вес и рост человека.
+#endregion
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите ваш рост");
+            var a = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Введите ваш вес");
+            var b = Convert.ToInt32(Console.ReadLine());
+
+            var sum = a + b;
+
+            Console.WriteLine($"Вы прошли анкету");
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ Program1.cs
@@ -0,0 +1,36 @@
+﻿#region Автор
+// Егор Малов
+#endregion
+
+#region Описание задачи
+// Написать программу обмена значениями двух переменных.
+//а) с использованием третьей переменной;
+#endregion
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 4;
+            int b = 3;
+            int c = 4;
+            int temp = a;
+            a = b;
+            c = a;
+            b = temp;
+            a = temp;
+
+            Console.WriteLine($"{a}, {b}, {c}");
+
+            Console.ReadLine();
+        }
+    }
+}
