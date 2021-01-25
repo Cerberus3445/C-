22b20094a93be4b52429c0a11af49fa7606281d5
Cerberus3445 <77907751+Cerberus3445@users.noreply.github.com>
2021-01-25 15:18:25 +1000

    Add files via upload

---
+++ Console555.cs
@@ -0,0 +1,45 @@
+﻿#region Автор
+//Малов Егор
+#endregion
+
+#region Описание задачи
+//Написать программу «Анкета». Последовательно задаются вопросы (имя, фамилия, возраст, рост, вес).
+#endregion
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Анкета в военкомат");
+            Console.WriteLine("Введите ваше имя");
+            string myname;
+            myname = Console.ReadLine();
+            Console.WriteLine("Следующий вопрос" +  myname);
+            Console.WriteLine("Введите вашу фамилию");
+            string surname;
+            surname = Console.ReadLine();
+            Console.WriteLine("Следующий вопрос" +  surname);
+            Console.WriteLine("Введите ваш возраст");
+            string age;
+            age = Console.ReadLine();
+            Console.WriteLine("Следующий вопрос" +  surname);
+            Console.WriteLine("Введите ваш рост");
+            string growth;
+            growth = Console.ReadLine();
+            Console.WriteLine("Следующий вопрос" + surname);
+            Console.WriteLine("Введите ваш вес");
+            string weight;
+            weight = Console.ReadLine();
+            Console.WriteLine($"Спасибо, что прошли анкету"); 
+            Console.ReadLine();
+        }
+    }
+}
