8b2d5149cc2eac9132434c6a2a412ea687de5cca
Cerberus3445 <77907751+Cerberus3445@users.noreply.github.com>
2021-01-25 15:40:47 +1000

    Add files via upload

---
+++ Programka.cs
@@ -0,0 +1,27 @@
+﻿#region Автор
+//Малов Егор
+#endregion
+
+#region Описание задачи
+//а) Написать программу, которая выводит на экран ваше имя, фамилию и город проживания.
+//б) Сделать задание, только вывод организуйте в центре экрана
+#endregion
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp777
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.SetCursorPosition(Console.WindowWidth / 2, Console.WindowHeight / 2);
+            Console.WriteLine("Вас зовут Егор, фамилия Малов,вы живете в Владивостоке");
+            Console.ReadLine();
+        }
+    }
+}
